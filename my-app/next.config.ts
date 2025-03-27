0b39ae362ebba1e3a984296b79b1e8aa2222ae83
Boojana <boojanashree.g@appteq.in>
2025-03-27 14:55:52 +0530

    Revert "jenkins test build, unit testing 2"
    This reverts commit 71e16d84e4e5269fbcf9015733c6dd7991aa88b3.

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,8 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    forceSwcTransforms: true, // Forces SWC to be used even with Babel
-  }
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
