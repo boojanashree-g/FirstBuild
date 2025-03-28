606589d5a91c9434e9b4d9d84167f5b3748ae7f8
Boojana <boojanashree.g@appteq.in>
2025-03-28 10:36:32 +0530

    build test 2

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    forceSwcTransforms: true, // Forces Next.js to use SWC even if Babel is present
-  },
 };
 
 export default nextConfig;
